13193276a1da4482ea1a4ba7d872053f914c2006
Michał Kostrzyński <michal@kostrzynski.pl>
2017-11-23 16:10:31 +0100

    Clean up package.json and fix export.

--- src/fluidContainer.tsx
+++ src/fluidContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { Motion, spring, presets, OpaqueConfig } from 'react-motion';
 import Measure, { ContentRect } from 'react-measure';
 
